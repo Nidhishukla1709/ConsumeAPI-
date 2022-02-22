31af4e9408873308020183966d306b7d64ae48f6
Nidhi Shukla <43289190+Nidhishukla1709@users.noreply.github.com>
2022-02-22 21:56:28 +0530

    Add files via upload

---
+++ Controllers/NewApiController.cs
@@ -0,0 +1,30 @@
+﻿using MyNewAspWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyNewAspWebAPI.Controllers
+{
+    public class NewApiController : ApiController
+    {
+    
+        PRATICEEntities db = new PRATICEEntities();
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Index()
+        {
+            List<student> obj = db.students.ToList();
+            return Ok(obj);
+        }
+
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult Index(int id)
+        {
+            var obj = db.students.Where(model => model.std_id == id).FirstOrDefault();
+            return Ok(obj);
+        }
+    }
+}
